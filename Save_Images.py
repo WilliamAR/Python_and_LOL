6c107f8cf1fb628ba0ed0c12b2b6f35070ca537e
WilliamAR <47059623+WilliamAR@users.noreply.github.com>
2020-06-08 11:40:10 -0500

    Add Save_images
    Web scraper a los campeones de League of Legends almacenados en una carpeta ya creada

---
+++ Save_Images.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib
+import os
+from re import sub, search
+
+URL = 'https://lan.leagueoflegends.com/es-mx/champions/'
+
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find(id='___gatsby')
+
+find_div_imgs = results.find('div', class_='style__List-ntddd-2 fqjuPM')
+
+find_a_imgs = find_div_imgs.findAll('a')
+
+for find_div_img in find_div_imgs:
+    image_text = find_div_img.text
+    if search("\W", image_text):
+        sub(r'(.*)\W(.*)',r'\1\2',image_text)
+    image_url = find_div_img.find('img')['src']
+    print('Almacenando la imagen de: {}'.format(image_text))
+    urllib.request.urlretrieve(
+        image_url, os.path.join('imagenes', image_text + '.png'))
