1c3423c61632735474613b07ed88091839abbf20
WilliamAR <47059623+WilliamAR@users.noreply.github.com>
2020-06-09 16:12:37 -0500

    feat: metodos de la clase
    Se crearon métodos y recurrir de buena manera a la encapsulación.

--- Organizar_bases.py
+++ Organizar_bases.py
@@ -10,58 +10,125 @@ class OrganizarBase(object):
         Las bases contienen: 
             Baneos, Oro, Asesinatos, Información de la partida, Monstruos y
             estructuras.
-        '''
-    
-    def arreglar_base(self):
-        
-        '''
-        arreglar_base()
-        
-        Return:
-            Carga todas las bases suministradas de Kaggle del siguiente
+            
+        Carga todas las bases suministradas de Kaggle del siguiente
             enlace https://www.kaggle.com/chuckephron/leagueoflegends/metadata 
             y modificados segúnla conveniencia de las gráficas o el tipo de 
             análisis.
         '''
         
-        #Base de datos bans
+    def baneo(self):
+        
+        '''
+        Return:
+            Base de datos que contiene el baneo durante las partidas.
+        '''
+        
         lol_bans = pd.read_csv('bans.csv')
         
-        #Base de datos oro
+        return lol_bans
+    
+    def oro(self):
+        
+        '''
+        Return:
+            Base de datos que contiene el oro durante las partidas.
+        '''
+        
         lol_gold = pd.read_csv('gold.csv')
         
-        #Base ded datos muertes
+        return lol_gold
+    
+    def muerte(self):
+        
+        '''
+        Return:
+            Base de datos que contiene las muertes durante las partidas.
+        '''
+        
         lol_kills = pd.read_csv('kills.csv')
         
-        #Base de datos, información
-        #lol_LOL = pd.read_csv('LeagueofLegends.csv')
+        return lol_kills
+    
+    def info_partida(self):
         
-        #Base de datos información de la partida
+        '''
+        Return:
+            Base de datos que contiene la información general durante las 
+            partidas.
+        '''
+        
         lol_match_info = pd.read_csv('matchinfo.csv')
         
-        #Base de datos monstruos
+        return lol_match_info
+    
+    def monstruo(self):
+        
+        '''
+        Return:
+            Base de datos que contiene la ejecución de los monstruos durante 
+            las partidas.
+        '''
+        
         lol_monsters = pd.read_csv('monsters.csv')
         
-        #Base de datos estructuras
+        return lol_monsters
+    
+    def estructura(self):
+        
+        '''
+        Return:
+            Base de datos que contiene la ejecución de las estructuras durante 
+            las partidas.
+        '''
+        
         lol_structures = pd.read_csv('structures.csv')
-            
-        ## ORGANIZAR LA BASE BANS!!
+        
+        return lol_structures
+    
+    def arreglar_baneo(self, lol_bans):
+        
+        '''
+        Parámetros:
+            lol_bans: DataFrame que contiene los baneos de las partidas.
+
+        Return:
+            DataFrame con algunas correcciones en la información.
+        '''
+        
         lol_bans = lol_bans.set_index(
             ['Address', 'Team']).stack().reset_index().rename(
                 columns = {0: 'Champion_ban', 'level_2': 'Number_ban', 
                             'Team': 'Team_ban'})
+                
+        return lol_bans
+    
+    def arreglar_oro(self, lol_gold):
         
-        ## ORGANIZAR LA BASE GOLD!!
+        '''
+        Parámetros:
+            lol_gold: DataFrame que contiene el oro de las partidas.
+
+        Return:
+            DataFrame con algunas correcciones en la información.
+        '''
+        
         lol_gold= lol_gold.set_index(['Address','Type']).stack().\
             unstack(level = 'Type').reset_index().rename(
                 columns = {'level_1':'Time_gold'})
-            
-        lol_gold.Time_gold = lol_gold.Time_gold.\
-            str.replace(r'(.*_)(.*)', r'\2').astype(int)
-        lol_gold = lol_gold.drop(labels = ['goldblue', 'goldred', 'golddiff'],
-                                 axis = 1)
+                
+        return lol_gold
+    
+    def arreglar_muerte(self, lol_kills):
         
-        ## ORGANIZAR LA BASE KILLS!!
+        '''
+        Parámetros:
+            lol_kills: DataFrame que contiene las muertes en las partidas.
+
+        Return:
+            DataFrame con algunas correcciones en la información.
+        '''
+        
         lol_kills = lol_kills.set_index(
             ['Address','Team', 'Time', 'x_pos', 'y_pos']).stack().reset_index().\
             rename(
@@ -76,19 +143,77 @@ class OrganizarBase(object):
         lol_kills.y_pos = lol_kills.y_pos.astype(int)
         
         lol_kills.Player = lol_kills.Player.str.replace(r'(.*\s)(.*)', r'\2')
+                
+        return lol_kills
+    
+    def arreglar_monstruo(self, lol_monsters):
         
-        ## ORGANIZAR LA BASE MONSTERS!!
+        '''
+        Parámetros:
+            lol_monsters: DataFrame que contiene las ejecuciones de los 
+            monstruos en las partidas.
+
+        Return:
+            DataFrame con algunas correcciones en la información.
+        '''
+        
         lol_monsters = lol_monsters.rename(
             columns = {'Team': 'Team_monster', 'Time': 'Time_monster', 
                         'Type': 'Type_monster'})
+                
+        return lol_monsters
+    
+    def arreglar_estructura(self, lol_structures):
         
-        ## ORGANIZAR LA BASE STRUCTURES!!
+        '''
+        Parámetros:
+            lol_monsters: DataFrame que contiene las ejecuciones de los 
+            monstruos en las partidas.
+
+        Return:
+            DataFrame con algunas correcciones en la información.
+        '''
+        
         lol_structures = lol_structures.rename(
             columns = {'Team': 'Team_structure', 'Time': 'Time_structure', 
                         'Type': 'Type_structure'})
+                
+        return lol_structures
+    
+    def base(self):
         
-        return lol_bans, lol_gold, lol_kills, lol_match_info, lol_monsters, lol_structures
-
-
-
-
+        '''
+        Return:
+            Devuelve todas las bases de League of Legends en DataFrame 
+            separados.
+        '''
+        
+        OB = OrganizarBase()
+        b = OB.baneo()
+        o = OB.oro()
+        m = OB.muerte()
+        ip = OB.info_partida()
+        mo = OB.monstruo()
+        e = OB.estructura()
+        
+        return b, o, m, ip, mo, e
+        
+        
+    
+    def arreglar_base(self):
+        
+        '''
+        Return:
+            Devuelve todas las bases de League of Legends en DataFrame 
+            separados con las modificaciones prefijadas para cada DataFrame.
+        '''
+        OB = OrganizarBase()
+        b, g, k, mi, m, s = OB.base()
+        
+        b = OB.arreglar_baneo(b)
+        g = OB.arreglar_oro(g)
+        k = OB.arreglar_muerte(k)
+        m = OB.arreglar_monstruo(m)
+        s = OB.arreglar_estructura(s)
+        
+        return b, g, k, mi, m, s
